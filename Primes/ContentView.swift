f83e382f00d99730745b83e2740b10695c0fad1c
Dan Wallace <daniel.wallace.nz@gmail.com>
2019-09-13 11:01:49 +1200

    Initial Commit

---
+++ Primes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Primes
+//
+//  Created by Daniel Wallace on 13/09/19.
+//  Copyright © 2019 danwallacenz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

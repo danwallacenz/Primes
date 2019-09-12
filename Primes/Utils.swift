1596259989fde754b39bb9f957713828a537e189
Dan Wallace <daniel.wallace.nz@gmail.com>
2019-09-13 11:07:17 +1200

    init

---
+++ Primes/Utils.swift
@@ -0,0 +1,84 @@
+//
+//  Utils.swift
+//  Primes
+//
+//  Created by Daniel Wallace on 13/09/19.
+//  Copyright © 2019 danwallacenz. All rights reserved.
+//
+
+import Foundation
+
+public func ordinal(_ n: Int) -> String {
+    let formatter = NumberFormatter()
+    formatter.numberStyle = .ordinal
+    return formatter.string(for: n) ?? ""
+}
+
+public extension Int {
+    var isPrime: Bool {
+        guard self >= 2     else { return false }
+        guard self != 2     else { return true  }
+        guard self % 2 != 0 else { return false }
+        return !stride(from: 3, through: Int(sqrt(Double(self))), by: 2).contains { self % $0 == 0 }
+    }
+}
+
+extension Int: Identifiable {
+    public var id: Int {
+        self
+    }
+}
+
+public struct WolframAlphaResult: Decodable {
+    let queryresult: QueryResult
+
+    struct QueryResult: Decodable {
+        let pods: [Pod]
+
+        struct Pod: Decodable {
+            let primary: Bool?
+            let subpods: [SubPod]
+
+            struct SubPod: Decodable {
+                let plaintext: String
+            }
+        }
+    }
+}
+
+public func wolframAlpha(query: String, callback: @escaping (WolframAlphaResult?) -> Void) -> Void {
+    var components = URLComponents(string: "https://api.wolframalpha.com/v2/query")!
+    
+    let wolframAlphaApiKey = "UK8KWW-VHR4KGTU9Y"
+    components.queryItems = [
+        URLQueryItem(name: "input", value: query),
+        URLQueryItem(name: "format", value: "plaintext"),
+        URLQueryItem(name: "output", value: "JSON"),
+        URLQueryItem(name: "appid", value: wolframAlphaApiKey),
+    ]
+
+    URLSession.shared.dataTask(with: components.url(relativeTo: nil)!) { data, response, error in
+        callback(
+            data
+                .flatMap { try? JSONDecoder().decode(WolframAlphaResult.self, from: $0) }
+        )
+    }
+    .resume()
+}
+
+public func nthPrime(_ n: Int, callback: @escaping (Int?) -> Void) -> Void {
+  wolframAlpha(query: "prime \(n)") { result in
+    callback(
+      result
+        .flatMap {
+          $0.queryresult
+            .pods
+            .first(where: { $0.primary == .some(true) })?
+            .subpods
+            .first?
+            .plaintext
+        }
+        .flatMap(Int.init)
+    )
+  }
+}
